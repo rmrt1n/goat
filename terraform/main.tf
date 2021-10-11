8a33a665c867abf1889ee88e539253d93cfac939
Ryan Martin <51780559+rmrt1n@users.noreply.github.com>
2021-10-12 01:05:00 +0800

    updated terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,78 @@
 provider "aws" {
-  region = "ap-southeast-1"
+  region = var.region
 }
 
-resource "aws_ecr_repository" "ecr" {
-  name = "goat-registry"
-  image_tag_mutability = "MUTABLE"
+# ecr
+resource "aws_ecr_repository" "goat_ecr" {
+  name = "goat-ecr"
+  tags = local.tags
+}
 
-  image_scanning_configuration {
-    scan_on_push = true
+# dockerrun config file
+# this file should be zipped. see data.tf
+resource "local_file" "dockerrun" {
+  content = jsonencode({
+    AWSEBDockerrunVersion = "1"
+    Image = {
+      Name = "${local.ecr_url}/${var.container_image_name}"
+      Update = "true"
+    }
+    Ports = {
+      ContainerPort = var.container_port
+      HostPort = "80"
+    }
+  })
+  filename = "${path.module}/Dockerrun.aws.json"
+}
+
+# s3 bucket to store dockerrun file
+resource "aws_s3_bucket" "dockerrun_bucket" {
+  bucket = "goat-s3-dockerrun-bucket"
+  acl = "private"
+
+  versioning {
+    enabled = true
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = local.tags
 }
 
-resource "aws_elastic_beanstalk_application" "goat" {
+# bucket object
+resource "aws_s3_bucket_object" "dockerrun_object" {
+  key = "dockerrun.zip"
+  bucket = aws_s3_bucket.dockerrun_bucket.id
+  # source = data.archive_file.dockerrun_zip.output_path
+  source = local_file.dockerrun.filename
+  tags = local.tags
+}
+
+# eb application
+resource "aws_elastic_beanstalk_application" "eb_app" {
   name = "goat"
   description = "github clone"
+  tags = local.tags
 }
 
-resource "aws_elastic_beanstalk_environment" "goat_env" {
-  name = "goat"
-  application = aws_elastic_beanstalk_application.goat.name
+# eb app version
+resource "aws_elastic_beanstalk_application_version" "eb_app_ver" {
+  name = var.app_version
+  bucket = aws_s3_bucket.dockerrun_bucket.id
+  key = local_file.dockerrun.filename
+  tags = local.tags
+}
+
+# eb application environment uses default instance profile for eb
+resource "aws_elastic_beanstalk_environment" "eb_app_env" {
+  name = "goat-env"
+  application = aws_elastic_beanstalk_application.eb_app.name
   solution_stack_name = "64bit Amazon Linux 2 v3.4.7 running Docker"
 
   setting {
@@ -26,4 +80,6 @@ resource "aws_elastic_beanstalk_environment" "goat_env" {
     name = "IamInstanceProfile"
     value = "aws-elasticbeanstalk-ec2-role"
   }
+
+  tags = local.tags
 }
