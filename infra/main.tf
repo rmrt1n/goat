0be46e846b868d4694dc8b3bc22a2201c07f174a
Ryan Martin <51780559+rmrt1n@users.noreply.github.com>
2021-10-12 21:16:52 +0800

    comment out problem line

--- infra/main.tf
+++ infra/main.tf
@@ -87,7 +87,9 @@ resource "aws_elastic_beanstalk_environment" "eb_app_env" {
   name                = "goat-env"
   application         = aws_elastic_beanstalk_application.eb_app.name
   solution_stack_name = "64bit Amazon Linux 2 v3.4.7 running Docker"
-  version_label       = aws_elastic_beanstalk_application_version.eb_app_ver.name
+
+  # this doesn't work for now
+  # version_label       = aws_elastic_beanstalk_application_version.eb_app_ver.name
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
