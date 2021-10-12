5b325873df75aad442164f6b4b5a56ab54d3f5e3
Ryan Martin <51780559+rmrt1n@users.noreply.github.com>
2021-10-12 19:13:57 +0800

    not sure why app is not showing

--- terraform/main.tf
+++ terraform/main.tf
@@ -85,6 +85,7 @@ resource "aws_elastic_beanstalk_environment" "eb_app_env" {
   name                = "goat-env"
   application         = aws_elastic_beanstalk_application.eb_app.name
   solution_stack_name = "64bit Amazon Linux 2 v3.4.7 running Docker"
+  version_label       = aws_elastic_beanstalk_application_version.eb_app_ver.name
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
